f3884902a0f3b3f87de76814b08cd454ea1f30a0
BaiFanJiuShuang <35549934+BaiFanJiuShuang@users.noreply.github.com>
2018-10-11 18:19:29 +0800

    Add files via upload

---
+++ IP_Agent.py
@@ -0,0 +1,109 @@
+import urllib.request
+import urllib
+import requests
+import re
+import time
+import random
+import threading
+import socket
+import ast
+
+proxys = []
+
+def show_interface():
+    choice=input("请选择功能：1.显示可用代理；2.显示可用代理并生成文本；3.退出系统:")
+    return choice
+
+def get_proxyIP():
+    ip_title=[]#IP列表
+    try:
+        page_num=input('想要爬取多少个代理IP页面(注：每个页面包含100个IP地址,输入数值大于等于2，小于等于705)：')
+        if int(page_num)<2 or int(page_num)>705:
+            print(">>>>>已经说明清楚了，再乱输的是傻逼   o -_-)=○)°O°) 给你一拳！")
+            return ip_title
+        else:
+            for i in range(1,int(page_num)):
+                url='http://www.xicidaili.com/nt/'+str(i)
+                headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64)"}
+                request=urllib.request.Request(url=url,headers=headers)
+                response=urllib.request.urlopen(request)
+                content=response.read().decode('utf-8')
+                pattern=re.compile('<td>(\d.*?|HTTP|HTTPS)</td>')
+                ip_page=re.findall(pattern,str(content))
+                ip_title.extend(ip_page)
+                time.sleep(random.choice(range(1,3)))
+            print('代理IP地址              ','\t','端口','\t','类型','\t\t\t','存活时间','\t\t','验证时间')
+            for i in range(0,len(ip_title),5):
+                if len(ip_title[i])<14 and len(ip_title[i])>10:
+                    print(ip_title[i],'           ','\t',ip_title[i+1],'\t',ip_title[i+2],'\t\t\t',ip_title[i+3],'\t\t',ip_title[i+4])
+                elif len(ip_title[i])<=10:
+                    print(ip_title[i], '             ', '\t', ip_title[i + 1],'\t', ip_title[i + 2],'\t\t\t', ip_title[i + 3],'\t\t',ip_title[i+4])
+                else:
+                    print(ip_title[i],'          ','\t',ip_title[i+1],'\t',ip_title[i+2],'\t\t\t',ip_title[i+3],'\t\t',ip_title[i+4])
+            return ip_title
+    except ValueError:
+        print(">>>>>已经说明清楚了，再乱输的是傻逼   o -_-)=○)°O°) 给你一拳！")
+        return ip_title
+
+proxy_ip=open('proxy_ip.txt','w')#新建文档存储有效IP
+
+def effective_IP(ip_title):
+    socket.setdefaulttimeout(5)#设置全局超时时间
+    url='https://www.cnblogs.com/sjzh/p/5990152.html'
+    try:
+        for i in range(0,len(ip_title),5):
+            ip={ip_title[i+2]:ip_title[i]+":"+ip_title[i+1]}
+            proxy_support=urllib.request.ProxyHandler(ip)
+            opener=urllib.request.build_opener(proxy_support)
+            opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64)')]
+            urllib.request.install_opener(opener)
+            res=urllib.request.urlopen(url).read()
+            print(ip_title[i]+':'+ip_title[i+1],'is OK')
+            proxy_ip.write('%s\n'%str(ip_title[i+2]+':'+'\''+ip_title[i]+':'+ip_title[i+1]+'\''))#写入IP
+        print('总共爬取了'+str(len(ip_title)/5)+'个有效IP')
+    except Exception as e:
+        print(i,e)
+
+if __name__=='__main__':
+    print('                                                                                                                      ')
+    print('                                                                                                                      ')
+    print('        ◆◆     ◆◆◆◆◆             ◆◆◆◆◆◆◆◆◆◆◆◆◆◆                                                  ')
+    print('        ◆◆      ◆◆  ◆◆◆              ◆    ◆◆    ◆      ◆          ◆◆◆◆        ◆◆◆◆◆◆◆          ')
+    print('        ◆◆        ◆      ◆◆            ◆    ◆      ◆      ◆           ◆◆◆           ◆◆◆◆◆◆◆◆      ')
+    print('      ◆◆◆        ◆      ◆◆◆          ◆    ◆◆◆◆◆◆◆◆◆            ◆◆            ◆◆    ◆◆◆◆      ')
+    print('      ◆◆◆◆◆◆◆◆◆◆◆◆◆◆          ◆    ◆      ◆      ◆            ◆◆            ◆◆      ◆◆◆      ')
+    print('    ◆◆◆          ◆◆                    ◆    ◆      ◆      ◆            ◆◆            ◆◆    ◆◆◆◆      ')
+    print('    ◆◆◆          ◆◆                ◆◆◆◆◆◆◆    ◆    ◆◆            ◆◆            ◆◆◆◆◆◆◆        ')
+    print('    ◆  ◆          ◆◆                    ◆    ◆◆◆◆◆◆◆◆◆            ◆◆            ◆◆◆◆◆◆          ')
+    print('        ◆          ◆◆                    ◆            ◆                    ◆◆            ◆◆                  ')
+    print('        ◆            ◆◆                  ◆            ◆                    ◆◆            ◆◆                  ')
+    print('        ◆            ◆◆      ◆          ◆    ◆◆◆◆◆◆◆◆◆            ◆◆            ◆◆                  ')
+    print('        ◆            ◆◆◆  ◆◆          ◆◆◆        ◆                ◆◆◆◆◆      ◆◆◆◆◆◆              ')
+    print('        ◆              ◆◆  ◆◆        ◆◆◆◆        ◆                                                          ')
+    print('        ◆                ◆◆◆◆    ◆◆◆◆  ◆◆◆◆◆◆◆◆◆◆◆                                                ')
+    print('        ◆                  ◆◆                                                                                      ')
+    print('')
+    print('')
+    choice = show_interface()
+    while True:
+        if choice=='1':
+            get_proxyIP()
+            i=input(">>>>>还想继续使用该系统？(Y/N):")
+            if i=='Y' or i=='y':
+                choice=show_interface()
+            else:
+                print('>>>>>欢迎再次使用*_*，我也要睡觉觉了(-_-)ZZZ')
+                break
+
+        if choice=='2':
+            ip_title=get_proxyIP()
+            effective_IP(ip_title)
+            i = input(">>>>>还想继续使用该系统？(Y/N):")
+            if i == 'Y' or i == 'y':
+                choice = show_interface()
+            else:
+                print('>>>>>欢迎再次使用*_*，我也要睡觉觉了(-_-)ZZZ')
+                break
+        if choice=='3':
+            print('>>>>>欢迎再次使用*_*，我也要睡觉觉了(-_-)ZZZ')
+            break
